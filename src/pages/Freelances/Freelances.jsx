db2ff6dc904ba59d67bb55e04edfc46cb9f04045
Pierre Fasce <lemonpiero@gmail.com>
2022-07-19 17:12:08 +0200

    application des proptypes

--- src/pages/Freelances/Freelances.jsx
+++ src/pages/Freelances/Freelances.jsx
@@ -1,7 +1,31 @@
+import Card from '../../components/Card/Card'
+
+const freelanceProfiles = [
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Devops',
+    },
+    {
+        name: 'John Doe',
+        jobTitle: 'Developpeur frontend',
+    },
+    {
+        name: 'Jeanne Biche',
+        jobTitle: 'Développeuse Fullstack',
+    },
+]
+
 function Freelances() {
     return (
         <div>
-            <h2>Freelances</h2>
+            <h1>Freelances</h1>
+            {freelanceProfiles.map((profile, index) => (
+                <Card
+                    key={`${profile.name}-${index}`}
+                    label={profile.jobTitle}
+                    title={profile.name}
+                />
+            ))}
         </div>
     )
 }
